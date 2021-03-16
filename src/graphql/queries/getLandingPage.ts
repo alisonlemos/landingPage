af191fbffe0a8e385019833f31a71e960e29ab87
Alison <alisongarantizado@gmail.com>
2021-03-16 11:15:44 -0400

    Add dynamic Header

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,12 +1,34 @@
 const GET_LANDING_PAGE = /*GraphQL*/ `
-  query GET_LANDING_PAGE {
-    landingPage{
-      logo {
-        alternativeText
+  fragment imageData on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageData
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
         url
+      }
+      image {
+        ...imageData
       }
     }
   }
-`
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+    }
+  }`
 
 export default GET_LANDING_PAGE
